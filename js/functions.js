72af15ce522325b27f8cc95ff288b3dbe5fef58f
mharley87 <mike.harley87@yahoo.com>
2012-01-13 12:53:43 -0500

    f

---
+++ js/functions.js
@@ -0,0 +1,52 @@
+// JavaScript Document
+
+
+
+var letter 
+
+while (!letter) { 
+	letter = prompt('write an alphabetical letter');
+}
+//(trace)
+console.log(letter);
+
+document.write(letter);
+
+for (var i = 0; i < 10; i++) {
+	document.write('<br>' + i);
+
+}
+
+function isNameCool () {
+	var name = prompt ('What is your name?');
+	
+
+
+isNameCool();
+
+var writeLetterMultipleTimes = function () {
+	for (var i = 0; i < 5; i++) {
+		document.write('<strong>mike</strong><br>');
+	if (letter == '') {
+		document.write('please write letter "A"');
+	}else{
+		document.write("I dont like your name");
+	}
+}
+};
+
+writeLtterMultipleTimes ();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// JavaScript Document
